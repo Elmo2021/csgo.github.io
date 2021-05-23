dc8279f352183ab8f5368f1e8e76dff5c2e41ef3
Elmo2021 <a.bouazra@seznam.cz>
2021-05-23 13:14:21 +0200

    finish

---
+++ javascript.js
@@ -0,0 +1,289 @@
+/*document.getElementById("Astralis").addEventListener("mouseover", function(event) {
+    document.getElementById("Astralis").style.opacity = "0.4";
+
+})
+document.getElementById("Astralis").addEventListener("mouseleave", function(event) {
+    document.getElementById("Astralis").style.opacity = "0.6";
+
+})
+document.getElementById("Navi").addEventListener("mouseover", function(event) {
+    document.getElementById("Navi").style.opacity = "0.4";
+
+})
+document.getElementById("Navi").addEventListener("mouseleave", function(event) {
+    document.getElementById("Navi").style.opacity = "0.6";
+
+})
+document.getElementById("Gambit").addEventListener("mouseover", function(event) {
+    document.getElementById("Gambit").style.opacity = "0.4";
+
+})
+document.getElementById("Gambit").addEventListener("mouseleave", function(event) {
+    document.getElementById("Gambit").style.opacity = "0.6";
+
+})
+document.getElementById("Virtus").addEventListener("mouseover", function(event) {
+    document.getElementById("Virtus").style.opacity = "0.4";
+
+})
+document.getElementById("Virtus").addEventListener("mouseleave", function(event) {
+    document.getElementById("Virtus").style.opacity = "0.6";
+
+})
+document.getElementById("Liquid").addEventListener("mouseover", function(event) {
+    document.getElementById("Liquid").style.opacity = "0.4";
+
+})
+document.getElementById("Liquid").addEventListener("mouseleave", function(event) {
+    document.getElementById("Liquid").style.opacity = "0.6";
+
+})
+document.getElementById("Heroic").addEventListener("mouseover", function(event) {
+    document.getElementById("Heroic").style.opacity = "0.4";
+
+})
+document.getElementById("Heroic").addEventListener("mouseleave", function(event) {
+    document.getElementById("Heroic").style.opacity = "0.6";
+
+})
+
+/*document.getElementById("Big").addEventListener("mouseover", function(event) {
+    document.getElementById("Big").style.opacity = "0.4";
+
+})
+document.getElementById("Faze").addEventListener("mouseleave", function(event) {
+    document.getElementById("Faze").style.opacity = "0.6";
+
+})
+document.getElementById("Furia").addEventListener("mouseover", function(event) {
+    document.getElementById("Furia").style.opacity = "0.4";
+
+})
+document.getElementById("Furia").addEventListener("mouseleave", function(event) {
+    document.getElementById("Furia").style.opacity = "0.6";
+
+})
+document.getElementById("G2").addEventListener("mouseover", function(event) {
+    document.getElementById("G2").style.opacity = "0.4";
+
+})
+document.getElementById("G2").addEventListener("mouseleave", function(event) {
+    document.getElementById("G2").style.opacity = "0.6";
+
+})
+document.getElementById("Complexity").addEventListener("mouseover", function(event) {
+    document.getElementById("Complexity").style.opacity = "0.4";
+
+})
+document.getElementById("Complexity").addEventListener("mouseleave", function(event) {
+    document.getElementById("Complexity").style.opacity = "0.6";
+
+})
+
+if (document.getElementById("main").src == "https://img-cdn.hltv.org/teamlogo/kixzGZIb9IYAAv-1vGrGev.svg?ixlib=java-2.1.0&s=8f9986a391fcb1adfbfff021b824a937") {
+    document.getElementById("Navi").style.opacity = "1";
+}
+
+function changeImageNavi() {
+    if (document.getElementById("Navi").src == "https://img-cdn.hltv.org/teamlogo/kixzGZIb9IYAAv-1vGrGev.svg?ixlib=java-2.1.0&s=8f9986a391fcb1adfbfff021b824a937") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/kixzGZIb9IYAAv-1vGrGev.svg?ixlib=java-2.1.0&s=8f9986a391fcb1adfbfff021b824a937";
+
+
+    }
+}
+
+function changeImageAstralis() {
+    if (document.getElementById("Astralis").src == "https://img-cdn.hltv.org/teamlogo/9bgXHp-oh1oaXr7F0mTGmd.svg?ixlib=java-2.1.0&s=f567161ab183001be33948b98c4b2067") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/9bgXHp-oh1oaXr7F0mTGmd.svg?ixlib=java-2.1.0&s=f567161ab183001be33948b98c4b2067";
+
+    }
+
+}
+
+function changeImageGambit() {
+    if (document.getElementById("Gambit").src == "https://img-cdn.hltv.org/teamlogo/pNV-lVdpvYZIkDwHdEXXg-.svg?ixlib=java-2.1.0&s=8b557b5b4d283208976340ef1bc44c76") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/pNV-lVdpvYZIkDwHdEXXg-.svg?ixlib=java-2.1.0&s=8b557b5b4d283208976340ef1bc44c76";
+    } else {
+        document.getElementById("Gambit").src = "https://img-cdn.hltv.org/teamlogo/pNV-lVdpvYZIkDwHdEXXg-.svg?ixlib=java-2.1.0&s=8b557b5b4d283208976340ef1bc44c76";
+    }
+
+
+}
+
+function changeImageVirtus() {
+    if (document.getElementById("Virtus").src == "https://img-cdn.hltv.org/teamlogo/yZ6Bpuui1rW3jocXQ68XgZ.svg?ixlib=java-2.1.0&s=f39be1d3e7baf30a4e7f0b1216720875") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/yZ6Bpuui1rW3jocXQ68XgZ.svg?ixlib=java-2.1.0&s=f39be1d3e7baf30a4e7f0b1216720875";
+
+    }
+}
+
+function changeImageLiquid() {
+    if (document.getElementById("Liquid").src == "https://img-cdn.hltv.org/teamlogo/JMeLLbWKCIEJrmfPaqOz4O.svg?ixlib=java-2.1.0&s=c02caf90234d3a3ebac074c84ba1ea62") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/JMeLLbWKCIEJrmfPaqOz4O.svg?ixlib=java-2.1.0&s=c02caf90234d3a3ebac074c84ba1ea62";
+
+    }
+}
+
+function changeImageHeroic() {
+    if (document.getElementById("Heroic").src == "https://img-cdn.hltv.org/teamlogo/Q6BGM_VQRz8Y9m3UgaqiUx.svg?ixlib=java-2.1.0&s=9424e99a6d77c8f154ea2d74175b3e17") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/Q6BGM_VQRz8Y9m3UgaqiUx.svg?ixlib=java-2.1.0&s=9424e99a6d77c8f154ea2d74175b3e17";
+
+    }
+}
+
+function changeImageG2() {
+    if (document.getElementById("G2").src == "https://static.wikia.nocookie.net/lolesports_gamepedia_en/images/7/77/G2_Esportslogo_square.png/revision/latest?cb=20210124113902") {
+        document.getElementById("main").src = "https://static.wikia.nocookie.net/lolesports_gamepedia_en/images/7/77/G2_Esportslogo_square.png/revision/latest?cb=20210124113902";
+
+    }
+}
+
+function changeImageFuria() {
+    if (document.getElementById("Furia").src == "https://img-cdn.hltv.org/teamlogo/mvNQc4csFGtxXk5guAh8m1.svg?ixlib=java-2.1.0&s=11e5056829ad5d6c06c5961bbe76d20c") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/mvNQc4csFGtxXk5guAh8m1.svg?ixlib=java-2.1.0&s=11e5056829ad5d6c06c5961bbe76d20c";
+
+    }
+}
+
+function changeImageComplexity() {
+    if (document.getElementById("Complexity").src == "https://img-cdn.hltv.org/teamlogo/R0CzydpyX02BnkAYhy3I89.svg?ixlib=java-2.1.0&s=8c5833d6069ef924fdbb2e220fefea00") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/R0CzydpyX02BnkAYhy3I89.svg?ixlib=java-2.1.0&s=8c5833d6069ef924fdbb2e220fefea00";
+
+    }
+}
+
+function changeImageFaze() {
+    if (document.getElementById("Faze").src == "https://img-cdn.hltv.org/teamlogo/SMhzsxzbkIrgqCOOKGRXlW.svg?ixlib=java-2.1.0&s=e6a9ce0345c7d703e5eaac14307f69aa") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/SMhzsxzbkIrgqCOOKGRXlW.svg?ixlib=java-2.1.0&s=e6a9ce0345c7d703e5eaac14307f69aa";
+
+    }
+}
+
+function changeImageVitality() {
+    if (document.getElementById("Vitality").src == "https://img-cdn.hltv.org/teamlogo/GAlByJtDTnkgbb9p_71SUL.png?ixlib=java-2.1.0&w=100&s=ddc5952ae492cbefb10fbe64471486b5") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/GAlByJtDTnkgbb9p_71SUL.png?ixlib=java-2.1.0&w=100&s=ddc5952ae492cbefb10fbe64471486b5";
+
+    }
+}
+
+function changeImageNip() {
+    if (document.getElementById("Nip").src == "https://img-cdn.hltv.org/teamlogo/-ttGATBV_P_HcZazxNNtIb.png?ixlib=java-2.1.0&w=100&s=8553ef1ff134f4171868cbfaf234836d") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/-ttGATBV_P_HcZazxNNtIb.png?ixlib=java-2.1.0&w=100&s=8553ef1ff134f4171868cbfaf234836d";
+
+    }
+
+}
+
+function changeImageHavu() {
+    if (document.getElementById("Havu").src == "https://img-cdn.hltv.org/teamlogo/bGYa-QzoimP8JH5hcdXcu3.svg?ixlib=java-2.1.0&s=70ceee130e1571052313c6f74c078aa3") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/bGYa-QzoimP8JH5hcdXcu3.svg?ixlib=java-2.1.0&s=70ceee130e1571052313c6f74c078aa3";
+
+    }
+}
+
+function changeImageExtra() {
+    if (document.getElementById("Extra").src == "https://img-cdn.hltv.org/teamlogo/qGJmW___da6KQjZWwhZOD6.png?ixlib=java-2.1.0&w=100&s=eebed9be54bb4b61a9f9ca462a938128") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/qGJmW___da6KQjZWwhZOD6.png?ixlib=java-2.1.0&w=100&s=eebed9be54bb4b61a9f9ca462a938128";
+
+    }
+}
+
+function changeImageFunplus() {
+    if (document.getElementById("Funplus").src == "https://img-cdn.hltv.org/teamlogo/Min7KyMY3tuB2xt-nPyM-O.svg?ixlib=java-2.1.0&s=b037f336aeb9b89b9b09d4a4671f9b61") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/Min7KyMY3tuB2xt-nPyM-O.svg?ixlib=java-2.1.0&s=b037f336aeb9b89b9b09d4a4671f9b61";
+
+    }
+}
+
+function changeImageMousesports() {
+    if (document.getElementById("Mousesports").src == "https://img-cdn.hltv.org/teamlogo/1YWxVoOcvOf3R9Z0-HWyeU.svg?ixlib=java-2.1.0&s=07f948624704c960960b962a9c0416c3") {
+        document.getElementById("main").src = "https://img-cdn.hltv.org/teamlogo/1YWxVoOcvOf3R9Z0-HWyeU.svg?ixlib=java-2.1.0&s=07f948624704c960960b962a9c0416c3";
+
+    }
+}*/
+
+let a = 0;
+let back = document.querySelector(".arrow1");
+let next = document.querySelector(".arrow2");
+let passing = document.querySelector(".pass");
+
+next.addEventListener("click", function(event) {
+    a++;
+
+    if (a == 1) {
+        passing.style.marginLeft = "-10%";
+    }
+    if (a == 2) {
+        passing.style.marginLeft = "-20%";
+    }
+    if (a == 3) {
+        passing.style.marginLeft = "-30%";
+    }
+    if (a == 4) {
+        passing.style.marginLeft = "-40%";
+    }
+    if (a == 5) {
+        passing.style.marginLeft = "-50%";
+    }
+    if (a == 6) {
+        passing.style.marginLeft = "-60%";
+    }
+    if (a == 7) {
+        passing.style.marginLeft = "-70%";
+    }
+    if (a == 8) {
+        passing.style.marginLeft = "-80%";
+    }
+    if (a == 9) {
+        passing.style.marginLeft = "-90%";
+    }
+    if (a == 10) {
+        passing.style.marginLeft = "0%";
+    }
+
+
+    passing.style.transition = "all 0.5s linear";
+})
+
+back.addEventListener("click", function(event) {
+    a--;
+
+    if (a < 0) {
+        a = 0;
+    }
+    if (a == 0) {
+        passing.style.marginLeft = "0%";
+    }
+    if (a == 1) {
+        passing.style.marginLeft = "-10%";
+    }
+    if (a == 2) {
+        passing.style.marginLeft = "-20%";
+    }
+    if (a == 3) {
+        passing.style.marginLeft = "-30%";
+    }
+    if (a == 4) {
+        passing.style.marginLeft = "-40%";
+    }
+    if (a == 5) {
+        passing.style.marginLeft = "-50%";
+    }
+    if (a == 6) {
+        passing.style.marginLeft = "-60%";
+    }
+    if (a == 7) {
+        passing.style.marginLeft = "-70%";
+    }
+    if (a == 8) {
+        passing.style.marginLeft = "-80%";
+    }
+    if (a == 9) {
+        passing.style.marginLeft = "-90%";
+    }
+    if (a == 10) {
+        passing.style.marginLeft = "-100%";
+    }
+
+    passing.style.transition = "all 0.5s linear";
+})
